346bea0cfe3754e5342de1c1d0c9f92056b627f0
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-07-31 03:04:33 +0600

    Problems Related to Array

---
+++ Array/Problems/FindingMissingElementsInAsequenceOf-N_Natural_Numbers.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+void Missing_Elements(int *arr,int n)
+{
+    int low=arr[0];
+    int high=arr[n-1];
+    int loop=high-low;
+    int diff=low-0;
+    int k=0;
+    int x=0;
+    for(int i=0;i<n;i++)
+    {
+
+        if((arr[i]-i)!=diff)
+        {
+                 x=(arr[i]-arr[i-1]);
+                 int Count=arr[i-1];
+
+
+                for(int j=1;j<=x-1;j++)
+                {
+                    cout<<++Count<<" is missing "<<endl;
+
+
+
+                }
+
+
+
+
+        }
+    }
+}
+
+
+int main()
+{
+    int n,i;
+    cin>>n;
+    int *arr=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    Missing_Elements(arr,n);
+
+}
