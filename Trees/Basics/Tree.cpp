f4dfd625982fc3b8dc0018ea79f0b08c50979fea
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-09-05 17:10:57 +0600

    5th sept 2020

---
+++ Trees/Basics/Tree.cpp
@@ -0,0 +1,37 @@
+
+/*
+Introduction:
+Tree is a collection of nodes and among those nodes,one node is
+taken as root node.Rest of the nodes are taken as disjoint subsets.
+Each subsets is a tree again or a subtree again.
+
+Root -> The first node of the tree.
+
+Parent -> A node is a parent to it's very next descendants.
+
+children -> descendants which are connected by a single edge with a node.
+
+Siblings -> Children of same parents.
+
+Descendants -> they are the sets of nodes which can be reached from
+a particular node downwards.
+
+Ancestors -> Between the path of two particular nodes,all other nodes
+along the path are called ancestors.
+
+Degree of a node -> Number of direct children of a particular node.
+
+Leaf Nodes -> Nodes with degree 0 are leaf nodes./External nodes./Terminal nodes
+
+Non-Leaf Nodes -> Nodes with degrees > 0 are non-leaf nodes/internal nodes/non-terminal nodes.
+
+Levels -> starts from 1 and Horizontally measured .
+
+Height -> Root is of height 0,So height of a tree starts from 0 onwards till the last reachable node.
+
+Forest -> A collection of tree is a forest.
+
+Binary tree -> Every node can have max true children min 0 children.
+
+
+
