65a34bb41dde0e348944b31e5694898b033a467f
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-10-30 02:15:57 +0600

    30th oct 2020

--- Brute Force/remove-comments-from-a-cpp-file.cpp
+++ Brute Force/remove-comments-from-a-cpp-file.cpp
@@ -1,27 +1,60 @@
 
 
 
-string deleteline;
+#include<bits/stdc++.h>
+using namespace std;
+int a=0;
+int main()
+{
+
+
 string line;
 
 ifstream fin;
 fin.open("example.txt");
 ofstream temp;
 temp.open("temp.txt");
-cout << "Which line do you want to remove? ";
-cin >> deleteline;
 
 
 
-while (getline(fin,line))
+
+while (!fin.eof())
 {
-    if (line != deleteline)
-    {
-    temp << line << endl;
-    }
+   getline(fin,line);
+   cout<<line<<endl;
+
+ for(int i=0;i<line.size();i++)
+ {
+     if(line[i]=='/' && line[i+1]=='/')
+     {
+         line.erase(i,line.size());
+         break;
+     }
+ }
+  if(line[0]=='/' && line[1]=='*')
+  {
+      line.erase();
+      a=1;
+  }
+  if(line.size()!=0 && a==1)
+  {
+      if(line[line.size()-2]=='*' && line[line.size()-1]=='/')
+      {
+          a=0;
+          line.erase();
+
+      }
+      else{
+        line.erase();
+      }
+  }
+
+  temp<<line<<endl;
 }
 
 temp.close();
 fin.close();
 remove("example.txt");
 rename("temp.txt","example.txt");
+
+}
