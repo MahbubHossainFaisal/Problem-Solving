714ac8e500c5a5c39bfb928cc739dca5d88bdab3
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-12-22 23:49:16 +0600

    22nd dec 2020

---
+++ Strings/Codeforces/Helpful-maths.cpp
@@ -0,0 +1,51 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    string s;
+    cin>>s;
+    int one=0,two=0,three=0;
+    for(int i=0;i<s.size();i=i+2)
+    {
+       if(s[i]==49){
+        one++;
+       }
+       if(s[i]==50){
+        two++;
+       }
+       if(s[i]==51){
+        three++;
+       }
+    }
+    //cout<<one<<endl<<two<<endl<<three<<endl;
+    int j=0;
+
+        if(one != 0){
+            while(one!=0){
+            s[j]='1';
+            j=j+2;
+            one--;
+            }
+        }
+        if(two != 0){
+            while(two!=0){
+            s[j]='2';
+            j=j+2;
+            two--;
+            }
+        }
+        if(three!=0){
+            while(three!=0){
+            s[j]='3';
+            j=j+2;
+            three--;
+            }
+        }
+
+    cout<<s;
+    return 0;
+}
+
