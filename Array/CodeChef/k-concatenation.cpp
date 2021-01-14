f34e5eb36bb421a488d8eae221f7610fed3270ef
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-14 13:25:55 +0600

    K-Concatenation

--- Array/CodeChef/k-concatenation.cpp
+++ Array/CodeChef/k-concatenation.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int kadanes(int *arr,int n)
+typedef long long ll;
+
+ll kadanes(int *arr,int n)
 {
-    int sum=0,best_sum=0;
+    ll sum=0,best_sum=0;
 
     for(int i=0;i<n;i++)
     {
@@ -18,6 +20,42 @@ int kadanes(int *arr,int n)
     return best_sum;
 }
 
+ll maxSubArray(int arr[],int n,int k)
+{
+    ll kadanes_sum = kadanes(arr,n);
+
+    if(k==1){
+        return kadanes_sum;
+    }
+
+    ll curr_prefix_sum =0, curr_suffix_sum=0;
+    ll max_prefix_sum=INT_MIN, max_suffix_sum = INT_MIN;
+
+    for(int i=0;i<n;i++)
+    {
+        curr_prefix_sum+=arr[i];
+        max_prefix_sum=max(curr_prefix_sum,max_prefix_sum);
+    }
+    ll total_sum= curr_prefix_sum;
+
+    for(int i=n-1;i>=0;i--)
+    {
+        curr_suffix_sum+=arr[i];
+        max_suffix_sum=max(max_suffix_sum,curr_suffix_sum);
+    }
+    ll ans;
+    if(total_sum<0)
+    {
+        ans = max(max_prefix_sum+max_suffix_sum,kadanes_sum);
+    } else {
+        ans = max(max_prefix_sum+max_suffix_sum+(total_sum*(k-2)),kadanes_sum);
+    }
+
+    return ans;
+
+
+}
+
 int main()
 {
    int t;
@@ -29,22 +67,16 @@ int main()
 
 
     int *arr = new int[n];
-    int *arr2 = new int[n*k];
+
 
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
     }
-    int i=0;
-    for(int j=0; j<n*k; j++)
-    {
-        arr2[j]=arr[i++];
-        if(i==n){
-            i=0;
-        }
-    }
-    cout<<kadanes(arr2,n*k)<<endl;
-   }
-   return 0;
+
+    cout<<maxSubArray(arr,n,k)<<endl;
+
+}
+    return 0;
 }
 
