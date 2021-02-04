18cd4d21ab88e18cfeabf92d683bada7049c4b63
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-02-04 21:32:10 +0600

    Accessing private properties by getters and setters

---
+++ Object Oriented Programming/Getters and Setters/main.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "studentInfo.cpp";
+
+int main()
+{
+    Student *s1 = new Student;
+
+    s1->setName("Faisal");
+    s1->setAge(50);
+
+    s1->display();
+
+
+
+
+}
