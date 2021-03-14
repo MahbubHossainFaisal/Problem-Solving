a15f693e7f48b891077d7f5f641c84bc2b393e9b
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-03-15 02:59:57 +0600

    Tree Height,Number of nodes in a speicific level/depth,preorder,postorder,destructuring

--- Trees/Tree/Tree.cpp
+++ Trees/Tree/Tree.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include "TreeClass.h";
-
+#include "TreeClass.h"
+ static int realHeight=0 ;
+ static int localHeight =1;
 
 TreeNode<int>* takeInputLevelWise()
 {
@@ -68,8 +69,118 @@ void printTree( TreeNode<int>* root){
     printTree(root->children[i]);
   }
 }
+void printTreeLevelWise(TreeNode<int>* root){
+    if(root==NULL){
+        return;
+    }
+    queue <TreeNode<int>*> nodes;
+    nodes.push(root);
+    while(!nodes.empty()){
+        TreeNode<int>* Front = nodes.front();
+        nodes.pop();
+        cout<<Front->data<<":";
+        for(int i=0;i<Front->children.size();i++){
+            cout<<Front->children[i]->data<<" ";
+            nodes.push(Front->children[i]);
+            if(i+1 != Front->children.size()){
+                cout<<", ";
+            }
+        }
+        cout<<endl;
+    }
 
+}
+
+int numOfNodes(TreeNode<int>* root){
+    if(root==NULL){
+        return 0;
+    }
+    int ans=1;
+    for(int i=0;i<root->children.size();i++){
+        ans+=numOfNodes(root->children[i]);
+    }
+    return ans;
+}
+
+int heightOfTree(TreeNode<int>* root)
+{
+    if(root==NULL)
+    {
+        return 0;
+    }
+    if(root->children.size()==0){
+        localHeight=1;
+    }
+
+    int recHeight=1;
+
+    for(int i=0;i<root->children.size();i++){
+        heightOfTree(root->children[i]);
+        localHeight+=1;
+        recHeight+=1;
+
+        realHeight= max(localHeight,realHeight); //Something wrong in calculating height.Not getting the proper value.
+
+
+    }
+
+
+    return realHeight;
+}
+
+//same depth nodes
+void NodesOfSameDepth(TreeNode<int>* root,int k)
+{
+    if(root==NULL){
+        return;
+    }
+    if(k==0){
+        cout<<root->data<<" ";
+    }
+    for(int i=0;i<root->children.size();i++){
+        NodesOfSameDepth(root->children[i],k-1);
+    }
+}
+
+void preOrder(TreeNode<int>* root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+ for(int i=0;i<root->children.size();i++){
+        preOrder(root->children[i]);
+    }
+}
+
+void postOrder(TreeNode<int>* root){
+    if(root==NULL){
+        return;
+    }
+
+ for(int i=0;i<root->children.size();i++){
+        postOrder(root->children[i]);
+
+    }
+    cout<<root->data<<" ";
+}
+
+//Tree nodes ----> 1 2 2 3 2 4 5 3 6 7 8 0 0 0 2 9 10 1 11 0 2 12 13 0 0 1 14 2 15 16 0 0
 int main(){
  TreeNode<int>* root= takeInputLevelWise();
- printTree(root);
+ printTreeLevelWise(root);
+
+ cout<<endl<<"Number of nodes: "<<numOfNodes(root)<<endl;
+ cout<<endl<<"Height of the Tree: "<<heightOfTree(root)<<endl;
+ cout<<endl<<"Nodes in level/Depth 2: "<<endl;
+ NodesOfSameDepth(root,2);
+ cout<<endl;
+ cout<<"PreOrder: "<<endl;
+ preOrder(root);
+ cout<<endl;
+ cout<<"PostOrder: "<<endl;
+ postOrder(root);
+ cout<<endl;
+
+ //Delete Tree;
+ delete root;
 }
