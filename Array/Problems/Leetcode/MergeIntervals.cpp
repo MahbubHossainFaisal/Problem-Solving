e37ab545a161d9ecbb653b26c426f4a078f3af76
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-04-11 10:37:38 +0600

    Merge Intervals mistake solved

--- Array/Problems/Leetcode/MergeIntervals.cpp
+++ Array/Problems/Leetcode/MergeIntervals.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int sz=0;
-        
+       sort(intervals.begin(),intervals.end());
         for(int i=0; i<intervals.size()-1;i++){
             int start1 = intervals[i][0];
             int end1 = intervals[i][1];
@@ -11,26 +11,25 @@ public:
             
             if(end1 >= start2){
                 int newEnd =max(end1,end2);
-                intervals[i][1] = newEnd;
-                intervals[i+1][0]= -1;
-                intervals[i+1][1]=-1;
-                sz++;
+                intervals[i][0]=-1;
+                intervals[i][1]=-1;
+                intervals[i+1][0]= start1;
+                intervals[i+1][1]=newEnd;
+                
             }
             
         }
         
-        vector<vector<int>> ans(intervals.size()-sz);
-        int k=0;
+        vector<vector<int>> ans;
         for(int i=0;i<intervals.size();i++){
-            ans[k]=vector<int>(2);
-            for(int j=0;j<2;j++){
-                if(intervals[i][j]!=-1){
-                    ans[k].push_back(intervals[i][j]);
-                }
-               
+            if(intervals[i][0] != -1 && intervals[i][1] != -1){
+                int s = intervals[i][0];
+                int e = intervals[i][1];
+                ans.push_back({s,e});
             }
-             k++;
-        }
+            }
+            
+        
         
         return ans;
     }
