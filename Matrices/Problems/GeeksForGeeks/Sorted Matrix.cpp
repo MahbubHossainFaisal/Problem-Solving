e43c1aaf3363beb2d8ac7c887fb30a71f59f0b10
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-05-07 20:43:54 +0600

    Sorted Matrix

---
+++ Matrices/Problems/GeeksForGeeks/Sorted Matrix.cpp
@@ -0,0 +1,47 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<vector<int>> sortedMatrix(int N, vector<vector<int>> Mat) {
+    
+        vector<int> v;
+        
+        for(int i=0;i<Mat.size();i++){
+            for(int j=0;j<Mat[0].size();j++){
+                v.push_back(Mat[i][j]);
+            }
+        }
+        
+        sort(v.begin(),v.end());
+        int k=0;
+        
+         for(int i=0;i<Mat.size();i++){
+            for(int j=0;j<Mat[i].size();j++){
+                Mat[i][j]=v[k++];
+            }
+        }
+        
+        return Mat;
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> v(N, vector<int>(N));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) cin >> v[i][j];
+        Solution ob;
+        v = ob.sortedMatrix(N, v);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) cout << v[i][j] << " ";
+            cout << "\n";
+        }
+    }
+} 
