dddad5d1828d3634c36611045fe6ef05a7f40189
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-05-22 00:36:55 +0600

    Binary search is sorted order is unknown

---
+++ Searching/Binary Search/Binary Search if sorted order of array is Unknown.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void BinarySearch(int arr[],int n,int x){
+	int start=0;
+	int end= n-1;
+
+	while(start<=end){
+		int mid = start+(end-start)/2;
+
+		if(arr[mid]==x){
+			cout<<mid;
+			break;
+		}
+		else if(arr[mid]<x){
+			if(arr[start] > arr[end])
+			end = mid-1;
+			else{
+				start=mid+1;
+			}
+		}
+		else{
+			if(arr[start] > arr[end]){
+				start=mid+1;
+			}
+			else{
+				end=mid-1;
+			}
+		}
+	}
+	if(start>end){
+		cout<<"Not found!";
+	}
+}
+
+
+int main()
+{
+	int arr[10] ={10,9,8,7,7,5,4,3,2,1};
+	BinarySearch(arr,10,7);
+}
