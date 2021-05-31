b316d52176533374bdfbf0ece6380104e0a524cd
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-06-01 02:29:45 +0600

    First and last occurrence of a number leetcode

--- Searching/Binary Search/Problems/First and last occurance of a number.cpp
+++ Searching/Binary Search/Problems/First and last occurance of a number.cpp
@@ -1,57 +1,60 @@
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    int lastOccurrence(vector<int>&nums, int target){
+        int start=0;
+        int end=nums.size()-1;
+        int res =-1;
+        
+        while(start<=end){
+           int mid= start + (end-start)/2;
+            
+            if(nums[mid] == target){
+                res = mid;
+                start= mid+1;
+            }
+            else if(nums[mid] < target){
+                start = mid + 1;
+                
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        
+      return res;
+    }
 
-int lastOccurrence(int arr[], int n,int x){
-	int start=0;
-	int end=n-1;
-	int res=-1;
-
-	while(start<=end){
-		int mid = start + (end-start)/2;
-
-		if(x == arr[mid]){
-			res = mid;
-			start= mid+1;
-		}
-		else if(arr[mid] < x){
-			start = mid+1;
-
-		}
-		else {
-			end = mid-1;
-		}
-	}
-	return res;
-}
-
-int firstOccurrence(int arr[], int n,int x){
-	int start=0;
-	int end=n-1;
-	int res=-1;
-
-	while(start<=end){
-		int mid = start + (end-start)/2;
-
-		if(x == arr[mid]){
-			res = mid;
-			end = mid-1;
-		}
-		else if(arr[mid] < x){
-			start = mid+1;
-
-		}
-		else {
-			end = mid-1;
-		}
-	}
-	return res;
-}
-
-int main()
-{
-	int arr[12] ={2,4,5,6,10,10,10,10,10,12,16,19};
-	int first =firstOccurrence(arr,12,16);
-	int last = lastOccurrence(arr,12,16);
-	cout<<"First Occurance at index--->"<<first<<endl;
-	cout<<"Last Occurance at index---->"<<last<<endl;
-}
+    
+    int firstOccurrence(vector<int>&nums, int target){
+        int start=0;
+        int end=nums.size()-1;
+        int res =-1;
+        
+        while(start<=end){
+           int mid= start+(end-start)/2;
+            
+            if(nums[mid] == target){
+                res=mid;
+                end=mid-1;
+            }
+            else if(nums[mid] < target){
+                start = mid + 1;
+                
+            }
+            else{
+                end = mid-1;
+            }
+        }
+       
+        return res;
+    }
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = firstOccurrence(nums,target);
+        int last = lastOccurrence(nums,target);
+        vector<int> v;
+        v.push_back(first);
+        v.push_back(last);
+        return v;
+    }
+};
