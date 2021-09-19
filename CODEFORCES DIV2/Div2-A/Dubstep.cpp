0e0d54795764071de05bbf65dbf3b4ccfa7882fb
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-09-19 17:24:03 +0600

    div2 A

---
+++ CODEFORCES DIV2/Div2-A/Dubstep.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    string s;
+    cin>>s;
+    string ans;
+
+    for(int i=0; i<s.size(); i++){
+        if(s[i]=='W' && s[i+1]=='U' && s[i+2]=='B'){
+                ans.push_back(' ');
+            
+            i+=2;
+        }
+        else{
+            ans.push_back(s[i]);
+        }
+    }
+    cout<<ans;
+
+}   
