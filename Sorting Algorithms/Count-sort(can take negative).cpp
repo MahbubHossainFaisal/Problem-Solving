4a1341ecb9fe3799b5b5e88e362602aae5f7c81a
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-09-28 23:50:14 +0600

    Count sort negative value

--- Sorting Algorithms/Count-sort(can take negative).cpp
+++ Sorting Algorithms/Count-sort(can take negative).cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 /*
-in count sort, 
+in count sort,
 we have to first take the input of elements in the array arr.
 Then, we have take the max value from the array.
 We will create then two new array with the max value + 1 .
@@ -13,7 +13,7 @@ Then in count2 we will store the sum of count2[i-1]+count1[i]
 Then we will create a new array in which we will store the sorted array elements.
 (Here that is res array)
 We will take a reverse loop in array arr.
-We will take element (suppose that element is x) 
+We will take element (suppose that element is x)
 one by one and use that as an index of count2 array.
 going to that index in count2 we will decrease it by one.
 then, the value we will find, that value will be the new index of res array.
@@ -25,38 +25,39 @@ int main()
 {
   int n;
   cin>>n;
-  int arr[n];
+  vector<int> arr(n);
   for(int i=0; i<n; i++){
     cin>>arr[i];
   }
 
-  int maxVal = INT_MIN;
-  
+  int maxVal = *max_element(arr.begin(),arr.end());
+  int minVal = *min_element(arr.begin(),arr.end());
+  int range = maxVal-minVal+1;
+  cout<<minVal<<" "<<maxVal<<" "<<range<<endl;
+
+  int count1[range]={0};
+
+
   for(int i=0; i<n; i++){
-    if(maxVal < arr[i]){
-      maxVal = arr[i];
-    }
-  } 
-
-  int count1[maxVal+1]={0};
-  int count2[maxVal+1]={0};
-
-  for(int i=0; i<=maxVal; i++){
-    count1[arr[i]]++;
+    count1[arr[i]-minVal]++;
   }
-  
-  for(int i=1; i<=maxVal; i++){
-    count2[i] = count2[i-1]+count1[i];
+
+  for(int i=1; i<range; i++){
+    count1[i]+=count1[i-1];
   }
 
   //new array
   int res[n]={0};
   for(int i=n-1; i>=0; i--){
     int index = arr[i];
-    count2[index]--;
-    res[count2[index]] = index;
+
+    res[count1[index-minVal]-1] = index;
+    count1[index-minVal]--;
   }
 
+  for(int i=0; i<n; i++){
+    arr[i] = res[i];
+  }
   for(int i=0; i<n; i++){
     cout<<res[i]<<" ";
   }
