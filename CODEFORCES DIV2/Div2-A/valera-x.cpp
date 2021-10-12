8e600828acf597f84c17f84091731abb83423113
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-10-12 21:33:58 +0600

    42: div2 404A

---
+++ CODEFORCES DIV2/Div2-A/valera-x.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main() {
+
+  int n;
+  cin>>n;
+
+  char arr[n][n];
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+      cin>>arr[i][j];
+    }
+  }
+
+  int j=0; int k=n-1;
+  bool flag = true;
+
+  for(int i=0; i<n; i++){
+    if(arr[i][j] == arr[i][k]){
+      j++;
+      k--;
+    }
+    else{
+      flag = false;
+      break;
+    }
+  }
+
+  if(flag){
+    cout<<"YES"<<endl;
+  }else{
+    cout<<"NO"<<endl;
+  }
+
+}
