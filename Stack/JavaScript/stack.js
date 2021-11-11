687124fb5cfce6b5c46c1b4af2e1b1eddf893b9f
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-11-12 02:47:03 +0600

    Stack JS

---
+++ Stack/JavaScript/stack.js
@@ -0,0 +1,72 @@
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  isEmpty(){
+      return this.length === 0;
+  }
+  peek() {
+      if(this.isEmpty()){
+          console.log('Stack is empty!')
+      }else{
+          console.log(this.top.value);
+      }
+  }
+  push(value){
+      let currNode = new Node;
+      currNode.value = value;
+      if(this.isEmpty()){
+          this.top = currNode;
+          this.bottom = currNode;
+          
+      }
+      else{
+          this.top.next = currNode;
+          this.top = currNode;
+          
+      }
+      this.length++;
+  }
+  pop(){
+      if(this.isEmpty()) return null;
+      let currNode = this.top ;
+      let pointer = this.bottom;
+      while(pointer.next && pointer.next !=this.top){
+        pointer = pointer.next;
+      }
+      currNode = null;
+      this.top = pointer;
+      this.length--;
+  }
+  //isEmpty
+}
+
+const myStack = new Stack();
+
+myStack.push(4);
+myStack.push(6);
+myStack.push(8);
+myStack.push(10)
+myStack.push(12);
+myStack.push(13);
+myStack.pop();
+myStack.peek();
+myStack.pop();
+myStack.peek();
+myStack.pop();
+myStack.peek();
+myStack.pop();
+myStack.peek();
+myStack.pop();
+myStack.peek();
+myStack.pop();
+myStack.peek();
