f0b0d52dcbd3bb9eb79bbfabff1d372ce42007bb
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2022-07-22 21:19:43 +0600

    Selection sort

---
+++ LeetCode/Selection_Sort.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void SelectionSort(int *arr,int n){
+   for(int i=0; i<n-1; i++){
+      int min = INT_MAX;
+      int index = i;
+      for(int j=i+1; j<n; j++){
+        // cout<<"arr: "<<arr[j]<<endl;
+         if(arr[j] < arr[index]){
+            min = arr[j];
+            index = j;
+         }
+      }
+      swap(arr[i],arr[index]);
+
+   }
+}
+
+int main(){
+   int n;
+   cin>>n;
+   int *arr = new int[n];
+
+   for(int i=0; i<n; i++){
+      cin>>arr[i];
+   }
+   SelectionSort(arr,n);
+
+   for(int i=0; i<n; i++){
+      cout<<arr[i]<<" ";
+   }
+   return 0;
+   
+}
